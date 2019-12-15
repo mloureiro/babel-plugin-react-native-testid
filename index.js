00ff8d2368781f72163b0e5a0de212ef2b72e92b
Marcos Loureiro <mloureiro.dev@gmail.com>
2019-12-15 13:08:01 +0100

    Add linter and formatter
    Signed-off-by: Marcos Loureiro <mloureiro.dev@gmail.com>

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 const buildMapTestIdFunction = t => originalAttribute =>
-  t.jsxSpreadAttribute(
-    t.callExpression(
-      t.arrowFunctionExpression(
-        [t.identifier('id')],
-        t.blockStatement([
-          t.ifStatement(
-            t.binaryExpression('!==', t.identifier('__DEV__'), t.identifier('true')),
-            t.returnStatement(t.objectExpression([]))
-          ),
-          t.returnStatement(t.objectExpression([
-            t.objectProperty(t.stringLiteral('testID'), t.identifier('testID')),
-            t.objectProperty(t.stringLiteral('accessibilityLabel'), t.identifier('testID')),
-            t.objectProperty(t.stringLiteral('accessible'), t.booleanLiteral(true)),
-          ]))
-        ])
-      ),
-      [originalAttribute]
-    )
-  )
+	t.jsxSpreadAttribute(
+		t.callExpression(
+			t.arrowFunctionExpression(
+				[t.identifier('id')],
+				t.blockStatement([
+					t.ifStatement(
+						t.binaryExpression(
+							'!==',
+							t.identifier('__DEV__'),
+							t.identifier('true')
+						),
+						t.returnStatement(t.objectExpression([]))
+					),
+					t.returnStatement(
+						t.objectExpression([
+							t.objectProperty(
+								t.stringLiteral('testID'),
+								t.identifier('testID')
+							),
+							t.objectProperty(
+								t.stringLiteral('accessibilityLabel'),
+								t.identifier('testID')
+							),
+							t.objectProperty(
+								t.stringLiteral('accessible'),
+								t.booleanLiteral(true)
+							)
+						])
+					)
+				])
+			),
+			[originalAttribute]
+		)
+	)
 
 module.exports = function composeTestId({ types }) {
-  const mapTestIdFunction = buildMapTestIdFunction(types);
+	const mapTestIdFunction = buildMapTestIdFunction(types)
 
-  return {
-    name: 'testId Wrapper',
-    visitor: {
-      JSXAttribute(path) {
-        if (path.node.name.name !== 'testID')
-          return
+	return {
+		name: 'testId Wrapper',
+		visitor: {
+			JSXAttribute(path) {
+				if (path.node.name.name !== 'testID') return
 
-        const value = types.isExpression(path.node.value)
-          ? path.node.value
-          : path.node.value.expression
+				const value = types.isExpression(path.node.value)
+					? path.node.value
+					: path.node.value.expression
 
-        path.replaceWith(mapTestIdFunction(value))
-      }
-    }
-  }
+				path.replaceWith(mapTestIdFunction(value))
+			}
+		}
+	}
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,9 @@
-const { default: pluginTester } = require('babel-plugin-tester/pure');
-const plugin =  require('..');
-const path = require('path');
+const { default: pluginTester } = require('babel-plugin-tester/pure')
+const plugin = require('..')
+const path = require('path')
 
 pluginTester({
-  plugin,
-  fixtures: path.join(__dirname, '__fixtures__'),
-  babelOptions: {
-    configFile: path.join(__dirname, 'babel.config.json')
-  }
+	plugin,
+	fixtures: path.join(__dirname, '__fixtures__'),
+	babelOptions: { configFile: path.join(__dirname, 'babel.config.json') }
 })
